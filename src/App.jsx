cc8a7cb0caeeea6b2fbdcf6b1c3e2f777c6135e5
sohamsinh31 <sohamb2019@gmail.com>
2023-03-11 20:27:20 +0530

    project structure changes

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+import {Home} from './Pages/Home'
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Hello from "./Hello";
+import Hello from "./App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
